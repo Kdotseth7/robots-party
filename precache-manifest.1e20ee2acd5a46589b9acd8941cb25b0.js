0991e080dfc3d0c5f16600b68a10e89e36deb593
Kushagra Seth <kseth919@gmail.com>
2020-04-08 22:43:13 +0530

    Updates

---
+++ precache-manifest.1e20ee2acd5a46589b9acd8941cb25b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82240b06ff4b0310498487e6767bd3f4",
+    "url": "/roboparty/index.html"
+  },
+  {
+    "revision": "0e2dfcb096d118e1da86",
+    "url": "/roboparty/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "a0288be560c03859c6ba",
+    "url": "/roboparty/static/css/main.63aa2ae6.chunk.css"
+  },
+  {
+    "revision": "0e2dfcb096d118e1da86",
+    "url": "/roboparty/static/js/2.d59645c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboparty/static/js/2.d59645c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0288be560c03859c6ba",
+    "url": "/roboparty/static/js/main.41155482.chunk.js"
+  },
+  {
+    "revision": "9f41475c8341c4d45a20",
+    "url": "/roboparty/static/js/runtime-main.73ccb0a6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboparty/static/media/SEGA.12a3db8b.woff"
+  }
+]);
